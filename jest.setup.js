72ce2c2280803c35faf5562ce4515b87904082c0
Rohan Balgotra <112085387+Simbaa815@users.noreply.github.com>
2024-08-07 22:54:21 +0530

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom'
+
+
+module.exports = {
+  // other configurations...
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
